b3fc4a33214f2c47a355a285d8ff0c0771d6cc32
pushmyandy <1805311490@qq.com>
2018-07-15 21:25:26 +0800

    "reboot"

--- js/mainPage/nav.js
+++ js/mainPage/nav.js
@@ -1,22 +1,18 @@
 $(function () {
-    !function HandleNavClick() {
-        let wrapperOne = $(".wrapperOne");
-        let wrapperTwo = $(".wrapperTwo");
-        let wrapperThree = $(".wrapperThree");
-        let navOne = $("#navOne");
-        let navTwo = $("#navTwo");
-        let navThree = $("#navThree");
-        navOne.on("click", function () {
-            wrapperOne.fadeIn(1000);
-            wrapperOne.siblings().css("display", "none");
-        });
-        navTwo.on("click", function () {
-            wrapperTwo.fadeIn(1000);
-            wrapperTwo.siblings().css("display", "none");
-        });
-        navThree.on("click", function () {
-            wrapperThree.fadeIn(1000);
-            wrapperThree.siblings().css("display", "none");
-        })
-    }()
+    $("#submit").on("click", function () {
+        let sub = $("#submit");
+        let info = $(".info");
+        let black = $(".blackList");
+        sub.addClass("active").siblings().removeClass("active");
+        info.removeClass("dis");
+        black.addClass("dis");
+    });
+    $("#blackName").on("click", function () {
+        let sub = $("#blackName");
+        let info = $(".info");
+        let black = $(".blackList");
+        sub.addClass("active").siblings().removeClass("active");
+        info.addClass("dis");
+        black.removeClass("dis");
+    })
 });
